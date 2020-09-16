b1e445bdfbbee62bc34639d0a04d0020b63f1bd1
Matt Raible <matt.raible@okta.com>
2020-09-16 17:57:56 -0600

    Fix environment variables

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,8 @@ const { CLIENT_ID, ISSUER, OKTA_TESTING_DISABLEHTTPSCHECK } = process.env;
 
 export default {
   oidc: {
-    clientId: `${CLIENT_ID}`,
-    issuer: `${ISSUER}`,
+    clientId: CLIENT_ID,
+    issuer: ISSUER,
     redirectUri: 'http://localhost:8080/login/callback',
     scopes: ['openid', 'profile', 'email'],
     testing: {
